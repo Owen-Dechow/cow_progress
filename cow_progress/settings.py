e3facb873b9d81b16eb6c04b5a6a8cd919d6518b
Owen Dechow <owen,dechow@gmail.com>
2023-05-27 18:06:48 -0400

    Update settings.py

--- cow_progress/settings.py
+++ cow_progress/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG") == "True"
+DEBUG = eval(env("DEBUG"))
 
 ALLOWED_HOSTS = eval(env("ALLOWED_HOSTS"))
 
